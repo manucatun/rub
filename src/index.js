be1b98a93f0cd45eddea6dff71f61bd1ba44c0c7
manucatun <manuelcatun10@gmail.com>
2024-06-20 15:40:11 -0600

    rub v8

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ const Bot = require("./structures/Client");
 new Bot();
 
 /* SERVER */
-const { createServer } = require("http");
+var http = require("http");
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Conectado correctamente a Vercel!");
-});
-
-server.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000/");
-});
+http
+  .createServer(function (req, res) {
+    res.write("Bot Conectado Correctamente");
+    res.end();
+  })
+  .listen(3001);
 /* SERVER */
