a48761dd86b7a853e8f309085ceee35f47688ec5
manucatun <manuelcatun10@gmail.com>
2024-06-21 12:25:45 -0600

    rub v8

--- src/index.js
+++ src/index.js
@@ -8,9 +8,23 @@ new Bot();
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Servidor encendido")
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
 });
 
-app.listen(3001, console.log("Servidor iniciado en el puerto 3001"))
+app.listen(port, () => {
+  console.log(`port runing in http://localhost:${port}`);
+});
 /* SERVER */
